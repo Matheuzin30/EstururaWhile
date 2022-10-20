e88b314ae38a93124038c0b05304be7575594a62
Matheuzin30 <mat860913@gmail.com>
2022-10-20 19:07:35 -0300

    Adicionar arquivos de projeto.

---
+++ EstururaWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+    
+    namespace Course
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um Número");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x >= 0.0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.WriteLine("Digite Outro Número: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("Número Negativo!");
+
+        }
+    }
+}
